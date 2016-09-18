99a641ddb08a250d7cde1f28dbd730113b117105
Giampaolo Bellavite <gpbellavite@gmail.com>
2016-09-18 15:54:48 +0200

    Complete test suite

--- test/WebpackCleanupPlugin.js
+++ test/WebpackCleanupPlugin.js
@@ -1,6 +1,9 @@
 import path from 'path';
-import { expect } from 'chai';
+import fs from 'fs';
 import webpack from 'webpack';
+import { expect } from 'chai';
+import { stub } from 'sinon';
+
 import WebpackCleanupPlugin from '../src/WebpackCleanupPlugin';
 
 describe('WebpackCleanupPlugin', () => {
@@ -10,13 +13,46 @@ describe('WebpackCleanupPlugin', () => {
     expect(plugin.options.preview).to.be.true;
     expect(plugin.options.exclude).to.eql(['a.txt']);
   });
-  it('', () => {
+
+  describe('when deleting files', () => {
+    let unlinkSync;
     const outputPath = path.resolve(__dirname, 'assets');
-    const compiler = webpack({
+    const webpackConfig = {
+      entry: path.join(__dirname, 'entry.js'),
       output: {
         path: outputPath,
       },
-      plugins: [new WebpackCleanupPlugin()],
+    };
+
+    beforeEach(() => { unlinkSync = stub(fs, 'unlinkSync'); });
+    afterEach(() => { unlinkSync.restore(); });
+
+    it('should delete the extraneous files from the output path', (done) => {
+      const compiler = webpack({
+        ...webpackConfig,
+        plugins: [new WebpackCleanupPlugin({ quiet: true })],
+      });
+      compiler.run(() => {
+        expect(unlinkSync).to.have.been.calledWith(`${outputPath}/a.txt`);
+        expect(unlinkSync).to.have.been.calledWith(`${outputPath}/b.txt`);
+        expect(unlinkSync).to.not.have.been.calledWith(`${outputPath}/bundle.js`);
+        expect(unlinkSync).to.have.been.calledWith(`${outputPath}/foo.json`);
+        expect(unlinkSync).to.have.been.calledWith(`${outputPath}/z.txt`);
+        done();
+      });
+    });
+
+    it('should not delete when previewing files', (done) => {
+      const compiler = webpack({
+        ...webpackConfig,
+        plugins: [
+          new WebpackCleanupPlugin({ preview: true }),
+        ],
+      });
+      compiler.run(() => {
+        expect(unlinkSync).to.not.have.been.called;
+        done();
+      });
     });
   });
 });
--- test/getFiles.js
+++ test/getFiles.js
@@ -7,27 +7,29 @@ const assetsPath = path.resolve(__dirname, 'assets');
 describe('getFiles', () => {
   it('get recursively the files in the output path', () => {
     const files = getFiles(assetsPath);
-    expect(files).to.have.length(6);
+    expect(files).to.have.length(7);
     expect(files).to.include(`${assetsPath}/folder/p.txt`);
     expect(files).to.include(`${assetsPath}/folder/q.txt`);
     expect(files).to.include(`${assetsPath}/a.txt`);
     expect(files).to.include(`${assetsPath}/b.txt`);
+    expect(files).to.include(`${assetsPath}/bundle.js`);
     expect(files).to.include(`${assetsPath}/foo.json`);
     expect(files).to.include(`${assetsPath}/z.txt`);
   });
   it('exclude files from array', () => {
     const files = getFiles(assetsPath, ['a.txt', 'b.txt']);
-    expect(files).to.have.length(4);
+    expect(files).to.have.length(5);
     expect(files).to.not.include(`${assetsPath}/a.txt`);
     expect(files).to.not.include(`${assetsPath}/b.txt`);
   });
   it('exclude files from glob', () => {
     const files = getFiles(assetsPath, ['folder/**/*.txt']);
-    expect(files).to.have.length(4);
+    expect(files).to.have.length(5);
     expect(files).to.not.include(`${assetsPath}/folder/p.txt`);
     expect(files).to.not.include(`${assetsPath}/folder/q.txt`);
     expect(files).to.include(`${assetsPath}/a.txt`);
     expect(files).to.include(`${assetsPath}/b.txt`);
+    expect(files).to.include(`${assetsPath}/bundle.js`);
     expect(files).to.include(`${assetsPath}/foo.json`);
     expect(files).to.include(`${assetsPath}/z.txt`);
   });
